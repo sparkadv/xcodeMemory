ca782595fdbbbf344ea92989b85ef636834c943a
Mehmet Selim Yılmazel <selim.yilmazel@gmail.com>
2018-05-16 23:14:12 +0300

    Initial Commit

---
+++ Xcode-Memory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode-Memory
+//
+//  Created by Kuzey on 16.05.2018.
+//  Copyright © 2018 Kuzey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
