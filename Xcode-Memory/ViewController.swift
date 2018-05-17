79e94b9da2966b19413a2b57705750feb40e9b47
Mehmet Selim Yılmazel <selim.yilmazel@gmail.com>
2018-05-17 11:20:50 +0300

    son yuklenen

--- Xcode-Memory/ViewController.swift
+++ Xcode-Memory/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var isimLabel: UILabel!
+    @IBOutlet weak var tarihLabel: UILabel!
+    @IBOutlet weak var alertLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        alertLabel.text = ""
+        let kayitliisim = UserDefaults.standard.object(forKey: "saklananisim")
+        let kayitlitarih = UserDefaults.standard.object(forKey: "saklanantarih")
+        
+        if let yeniisim = kayitliisim as? String {
+            isimLabel.text = "İsim : \(yeniisim)"
+        }
+        if let yenitarih = kayitlitarih as? String {
+            tarihLabel.text = "Doğum Günü : \(yenitarih)"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func saveButton(_ sender: Any) {
+    
+        if textField1.text == "" || textField2.text == ""  { alertLabel.text = "Lütfen Bilgi Giriniz!"
+            
+        }
+        else {
+            isimLabel.text = "İsim : \(textField1.text!)"
+            tarihLabel.text = "Doğum Günü : \(textField2.text!)"
+            UserDefaults.standard.set (textField1.text!, forKey: "saklananisim")
+            UserDefaults.standard.set (textField2.text!, forKey: "saklanantarih")
+            UserDefaults.standard.synchronize()
+            alertLabel.text = ""
+        }
     }
-
-
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        let kayitliisim = UserDefaults.standard.object(forKey: "saklananisim")
+        let kayitlitarih = UserDefaults.standard.object(forKey: "saklanantarih")
+        
+        if (kayitliisim as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "saklananisim")
+            UserDefaults.standard.synchronize()
+            isimLabel.text = "İsim :"
+        }
+        if (kayitlitarih as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "saklanantarih")
+            UserDefaults.standard.synchronize()
+            tarihLabel.text = "Doğum Günü :"
+            alertLabel.text = ""
+        }
+    }
 }
 
